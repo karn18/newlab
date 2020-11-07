d34eef2653bf9efd49889dec89f917800bb93874
Karn Tirasoontorn <karn@hey.com>
2020-11-07 14:53:29 +0700

    Update test case

--- test/sum.test.js
+++ test/sum.test.js
@@ -11,3 +11,14 @@ test('adds 1 + 2 to equal 3', () => {
   // Assert
   expect(3).toBe(result);
 })
+test('adds 2 + 2 to equal 4', () => {
+  // Arrange
+  let a = 2
+  let b = 2
+  
+  // Act
+  let result = sum(a, b)
+
+  // Assert
+  expect(4).toBe(result);
+})
